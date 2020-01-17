607552ca35610e7cb068350d46296d2b76b2d401
Your Name <adamkq@icloud.com>
2020-01-16 21:06:31 -0800

    Added filesave and refactored the code a bit. Added better console messages.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # onet-dataviz
 A project to scrape, tabulate, and display job data from the O*NET website, and possibly other websites. Non-commercial. 
+
+Intended Method:
+1. Create a pandas table with the following headings:
+	SOC code (6 digits, unique, e.g. for Nuclear Engineers: 17-2161), Job Name, Job Family, Job Zone, Avg Salary, Jobs Forecast, Chance of Automation
+2. Go to all occupations page: https://www.onetonline.org/find/zone?z=0&s=0
+3. For each job, follow the link provided and get the following:
+		Number of New Jobs Forecasted, Avg Salary, Job Zone, SOC code 
+4. Format this data into the pandas table.
+5. For each job, go to google and type in "site:https://willrobotstakemyjob.com/ <SOC code>"
+6. Follow the first result link (only one should appear)
+7. Obtain the chance of automation
+8. Add that chance to the corresponding column in the table
+9. Determine summary data:
+- Average wage non-college (Job Zones 1-3)
+- Average wage college (Job Zones 4-5)
+- Average ChanceAuto, non-college
+- Average ChanceAuto, college
+10. Format the raw data and summary data into a chart using matplotlib
--- onet_dataviz.py
+++ onet_dataviz.py
@@ -6,82 +6,78 @@ import pandas as pd
 import numpy as np
 from bs4 import BeautifulSoup
 
+def tabulateData(table):
+	'''
+	Returns a pandas dataframe given a suitable bs4 object
+	'''
+	jobFamilySkip = ["Military Specific"] # skip these for now; these are missing data
+	columnNames = ["SOC Code", "Job Name", "Job Family", "isBright", "isGreen"] # iterate to get: Job Zone, Avg Salary, Jobs Forecast, 
+	rows = []
+
+	for i, element in enumerate(table2):
+		if i == 0: # first entry is the table header, which is formatted differently
+			continue
+		tags = element.find_all("td")
+		allContents = []
+		for tag in tags:
+			allContents.extend(tag)
+
+		isBright = False # Occupation has 'bright outlook' designation
+		isGreen = False # Occupation has 'green' designation
+		SOC_code = str(allContents[0])
+		jobName = str(allContents[1].contents[0])
+		jobFamily = str(allContents[-1])
+		if jobFamily in jobFamilySkip:
+			continue
+
+		if len(allContents) > 4: # may be bright or green
+			for item in allContents:
+				if 'Tag' in str(type(item)):
+					if "Bright Outlook" in item.attrs.values():
+						isBright = True
+					if "Green" in item.attrs.values():
+						isGreen = True
+					
+		rows.append([SOC_code, jobName, jobFamily, isBright, isGreen])
 
 
-'''
-1. Create a pandas table with the following headings:
-	SOC code (6 digits, unique, e.g. for Nuclear Engineers: 17-2161), Job Name, Job Family, Job Zone, Avg Salary, Jobs Forecast, Chance of Automation
-2. Go to all occupations page: https://www.onetonline.org/find/zone?z=0&s=0
-3. For each job, follow the link provided and get the following:
-		Number of New Jobs Forecasted, Avg Salary, Job Zone, SOC code 
-4. Format this data into the pandas table.
-5. For each job, go to google and type in "site:https://willrobotstakemyjob.com/ <SOC code>"
-6. Follow the first result link (only one should appear)
-7. Obtain the chance of automation
-8. Add that chance to the corresponding column in the table
-9. Determine summary data:
-- Average wage non-college (Job Zones 1-3)
-- Average wage college (Job Zones 4-5)
-- Average ChanceAuto, non-college
-- Average ChanceAuto, college
-10. Format the raw data and summary data into a chart using matplotlib
-'''
+	jobData = pd.DataFrame(rows, columns = columnNames)
+	jobData.insert(jobData.shape[1], "Job Zone", 1)
+	jobData.insert(jobData.shape[1], "Avg Salary", 0)
+	jobData.insert(jobData.shape[1], "Job Forecast", 0)
+	jobData.insert(jobData.shape[1], "ChanceAuto", 0.1)
+	return jobData
 
+print("="*10, "O*NET Dataviz", "="*10)
+scrape = False
 onet_url = "https://www.onetonline.org/find/family?f=0&g=Go"
-response = requests.get(onet_url)
-if not (200 <= response.status_code < 300):
-	print("An error occurred when trying to fetch data from the O*NET website. Python Exiting.")
-	sys.exit()
+
+if (scrape):
+	print("Getting O*NET data...")
+	response = requests.get(onet_url)
+	if not (200 <= response.status_code < 300):
+		print("An error occurred when trying to fetch data from the O*NET website. Python Exiting.")
+		sys.exit()
+	else:
+		print("Response OK")
+
+	soup = BeautifulSoup(response.text, "html.parser")
+	table = soup.find("table", border="0")
+	table2 = table.find_all("tr")
+
+	jobData = tabulateData(table2)
+	jobData.to_csv('jobData.csv',index=False)
+
 else:
-	print("Response OK")
-
-soup = BeautifulSoup(response.text, "html.parser")
-table = soup.find("table", border="0")
-table2 = table.find_all("tr")
-columnNames = ["SOC Code", "Job Name", "Job Family", "isBright", "isGreen"] # iterate to get: Job Zone, Avg Salary, Jobs Forecast, 
-rows = []
-
-# get the column Names listed above
-# There is an option to download CSV data for this, but it does not include Bright/Green data
-for i, element in enumerate(table2):
-	if i == 0: # first entry is the table header, which is formatted differently
-		continue
-	tags = element.find_all("td")
-	allContents = []
-	for tag in tags:
-		allContents.extend(tag)
-
-	isBright = False # Occupation has 'bright outlook' designation
-	isGreen = False # Occupation has 'green' designation
-	SOC_code = str(allContents[0])
-	jobName = str(allContents[1].contents[0])
-	jobFamily = str(allContents[-1])
-	if jobFamily == "Military Specific": # skip these for now; these are missing data
-		continue
-
-	if len(allContents) > 4: # may be bright or green
-		for index, j in enumerate(allContents):
-			if 'Tag' in str(type(j)):
-				if "Bright Outlook" in j.attrs.values():
-					isBright = True
-				if "Green" in j.attrs.values():
-					isGreen = True
-				
-	rows.append([SOC_code, jobName, jobFamily, isBright, isGreen])
-
-
-jobData = pd.DataFrame(rows, columns = columnNames)
-print(jobData.shape)
-jobData.insert(jobData.shape[1], "Job Zone", 1)
-jobData.insert(jobData.shape[1], "Avg Salary", 0)
-jobData.insert(jobData.shape[1], "Job Forecast", 0)
-jobData.insert(jobData.shape[1], "ChanceAuto", 0.1)
-print(jobData)
+	print("Loading data from file...")
+	jobData = pd.read_csv('jobData.csv')
 
 for index, row in jobData.iterrows():
-	link = "https://www.onetonline.org/link/summary/" +row["SOC Code"
+	link = "https://www.onetonline.org/link/summary/" + row["SOC Code"]
 	# row = getSummaryData(link)
 	print(link)
 	if index == 4:
 		break
+
+input() # Press Enter to quit
 
